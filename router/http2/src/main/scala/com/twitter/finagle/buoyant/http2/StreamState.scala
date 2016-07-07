77824baf591f14db68f907726c4ca47e516278eb
Oliver Gould <ver@buoyant.io>
2016-07-07 16:38:21 +0000

    wip it good

--- router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/StreamState.scala
+++ router/http2/src/main/scala/com/twitter/finagle/buoyant/http2/StreamState.scala
@@ -7,6 +7,10 @@ import io.netty.handler.codec.http2.Http2Headers
 private[http2] sealed trait StreamState
 private[http2] object StreamState {
 
+  /**
+   * The stream has been created but no messages have been
+   * sent or received.
+   */
   object Idle extends StreamState
 
   case class Open(
@@ -14,9 +18,12 @@ private[http2] object StreamState {
     trailers: Promise[Option[Http2Headers]]
   ) extends StreamState
 
-  case class HalfClosedLocal(trailers: Promise[Option[Http2Headers]]) extends StreamState
+  object HalfClosedLocal extends StreamState
 
-  object HalfClosedRemote extends StreamState
+  case class HalfClosedRemote(
+    writer: Writer with Closable,
+    trailers: Promise[Option[Http2Headers]]
+  ) extends StreamState
 
   object Closed extends StreamState
 }
